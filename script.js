fe71d54566761451a54f5321078af40954cf280c
megalo999 <myc9078@nyu.edu>
2022-09-09 21:21:40 -0400

    tidying up

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function calculateScore() {
     } else if (score >= 77) {
         meaning = "The city is your campus! A typical NYU purity score."
     } else if (score >= 45) {
-        meaning = "Way to show NYU Pride!"
+        meaning = "Way to show NYU Pride! Go Violets!"
     } else if (score >= 9) {
         meaning = "Are you doing okay? The Wellness Exchange is your greatest mental health resource at NYU. "
     } else {
@@ -34,11 +34,10 @@ function calculateScore() {
 
     var scoreDiv = document.getElementById("score"); //Gets the score div which is where the score is displayed
     var meaningDiv = document.getElementById("meaning"); //Gets the score div which is where the score is displayed
+    var listDiv = document.getElementById("list");
     scoreDiv.innerHTML = "Your Score: " + score.toString() //Sets the div text to your score
     meaningDiv.innerHTML = meaning //Sets the div text to your meaning
-    scoreDiv.style.padding = "20px"
-    scoreDiv.style.fontFamily = "GothamMedium"
-    meaningDiv.style.padding = "10px"
+    listDiv.style.display = 'none';
     //document.getElementById("refresh").style.display="inline"; //Reveals the refresh page button
     document.body.scrollTop = 0; // Scrolls to top of the page for Safari
     document.documentElement.scrollTop = 0; // Scrolls to the top of the page for Chrome, Firefox, IE, and Opera
